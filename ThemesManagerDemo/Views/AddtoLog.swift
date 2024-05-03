47a2c88b6d9e8a0723b2d309aa2b2614b1679daf
Kit R. Oster <98862584+kitoster@users.noreply.github.com>
2024-05-03 16:12:46 -0400

    addtolog view updates

--- ThemesManagerDemo/Views/AddtoLog.swift
+++ ThemesManagerDemo/Views/AddtoLog.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct AddtoLog: View {
+    @EnvironmentObject private var themeManager: ThemeManager
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Today, Insert Date")
+                .foregroundColor(themeManager.selectedTheme.primaryThemeColor)
+                .background(Color.clear)
+                //Here I'll add a border with a color that's clear in dark mode but not in other modes.
+                .padding(.vertical, 20)
+            ScrollView {
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.")
+                        .padding()
+            }
+            .background(Color.clear)
+            .border(Color.blue, width: 1)
+            .frame(height: 400)
+        
+        }
+        .padding()
     }
 }
 
 struct AddtoLog_Previews: PreviewProvider {
     static var previews: some View {
         AddtoLog()
+        .environmentObject(ThemeManager())
     }
 }
