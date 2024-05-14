66a5aa2d6c058c837de5da06bb9111c4900e962d
Kit R. Oster <98862584+kitoster@users.noreply.github.com>
2024-05-14 19:17:07 -0400

    Added HomeView Notes button
    Added emergency HomeView notes button.

--- ThemesManagerDemo/Views/HomeView.swift
+++ ThemesManagerDemo/Views/HomeView.swift
@@ -5,15 +5,13 @@ struct HomeView: View {
     @EnvironmentObject private var themeManager: ThemeManager
     @State private var isForecastCollapsed = true
     @State private var isSummaryCollapsed = true
-    
-    //Priority: GPT said toolbar should be moved from ContentView
-    //to something that can be higher up and encompass a lot of pages. 
+    @State private var isNotesViewPresented = false // State variable to control the presentation of NotesView
     
     var body: some View {
         NavigationView {
             VStack {
                 NavigationLink(destination: AddtoLog()) {
-                    // Handle plus button action here
+                    // Plus button
                     Text("Plus")
                         .font(themeManager.selectedTheme.textTitleFont)
                         .foregroundColor(themeManager.selectedTheme.primaryThemeColor) // Adjust the color as needed
@@ -24,43 +22,53 @@ struct HomeView: View {
                         .border(themeManager.selectedTheme.primaryThemeColor, width:1)
                 }
                 
-                    
                 VStack(spacing:20) {
                     Collapsible(label: {
                         Text("Forecast")
                             .font(themeManager.selectedTheme.textTitleFont)
                             .foregroundColor(themeManager.selectedTheme.bodyTextColor)
-                        }, content: {
-                            Text("Start date projected for May 6.\nMost likely dates are May 2-10th.")
-                                .font(themeManager.selectedTheme.bodyTextFont)
-                        })
-                        .padding()
-                        .background(Color.clear) // Set the background color to transparent
-                        .cornerRadius(8)
-                        .border(themeManager.selectedTheme.primaryThemeColor, width: 2) // Set the border color and width
+                    }, content: {
+                        Text("Start date projected for May 6.\nMost likely dates are May 2-10th.")
+                            .font(themeManager.selectedTheme.bodyTextFont)
+                    })
+                    .padding()
+                    .background(Color.clear) // Set the background color to transparent
+                    .cornerRadius(8)
+                    .border(themeManager.selectedTheme.primaryThemeColor, width: 2) // Set the border color and width
                     
                     
                     Collapsible(label: {
                         Text("Summary")
                             .font(themeManager.selectedTheme.textTitleFont)
                             .foregroundColor(themeManager.selectedTheme.bodyTextColor)
-                        }, content: {
-                            Text("Summary content goes here.\nLorem ipsum.")
-                                .font(themeManager.selectedTheme.bodyTextFont)
-                        })
-                        .padding()
-                        .background(Color.clear) // Set the background color to transparent
-                        .cornerRadius(8)
-                        .border(themeManager.selectedTheme.primaryThemeColor, width: 2) // Set the border color and width
-                    }
-                                       
+                    }, content: {
+                        Text("Summary content goes here.\nLorem ipsum.")
+                            .font(themeManager.selectedTheme.bodyTextFont)
+                    })
+                    .padding()
+                    .background(Color.clear) // Set the background color to transparent
+                    .cornerRadius(8)
+                    .border(themeManager.selectedTheme.primaryThemeColor, width: 2) // Set the border color and width
+                }
                 .padding()
                 .background(Color.clear) // Set the background color to transparent
+            }
+            .navigationBarItems(trailing:
+                Button(action: {
+                    isNotesViewPresented.toggle() // Toggle the NotesView presentation
+                }) {
+                    Image(systemName: "note.text") // Notes icon
+                        .font(.title)
+                        .foregroundColor(themeManager.selectedTheme.primaryThemeColor) // Adjust the color as needed
+                }
+            )
+            .sheet(isPresented: $isNotesViewPresented) {
+                // NotesView
+                NotesView()
             }
         }
     }
 }
-
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
